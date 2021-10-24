72c7d509acebd681189b507c9faf79b2bbc217eb
akshayembedded <embedded.akshay@gmail.com>
2021-10-24 11:36:53 +0530

    list edited

--- list.py
+++ list.py
@@ -7,5 +7,21 @@ print(a[4])
 a[4]=234
 print(a)
 
-a.append("hello")
+
+b="qwertyy"
+a.append("123")
 print(a)
+print(a)
+print(b)
+print(a.count("123"))
+a.extend(b)
+print(a)
+print(a.index("123"))
+a.insert(2,4444)
+print(a)
+a.pop(6)
+print(a)
+a.remove(4444)
+print(a)
+
+g="jrtgrt"
