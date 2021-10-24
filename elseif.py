9d1f94f08cce5eee09a5cf477f8711e1600deb72
Akshay Kumar <akshaykumarpe@gmail.com>
2021-10-24 09:32:08 +0530

    list addeed

---
+++ elseif.py
@@ -0,0 +1,7 @@
+a=int(input("Enter a value"))
+if a<0:
+  print("Number is Negative")
+elif a>0:
+  print("Number is Positive")
+else:
+  print("Number is Zero")  
---
+++ list.py
@@ -0,0 +1,11 @@
+a=["qwe","asd",'erd','123',343,"LOIUY",6+4j]
+print(a)
+print(a[:3])
+print(a[2:5])
+print(a[-3:-1])
+print(a[4])
+a[4]=234
+print(a)
+
+a.append("hello")
+print(a)
