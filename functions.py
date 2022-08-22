fe3f0776e145650e4f4fea09c0a155b9ae912539
Akshay <embedded.akshay@gmail.com>
2022-08-22 15:58:03 +0530

    with function

---
+++ functions.py
@@ -0,0 +1,19 @@
+# defention
+# call
+#no arg, no return
+def testfn1(): #function def
+    print("Hello Its function")
+
+testfn1()
+
+################# with arg #################################
+
+def testfn2(a,b):
+    print("Its arg function\n Values are\na=",a)
+    print("b=",b)
+
+testfn2(15,63)
+testfn2(b=15,a=63)
+
+########### Default arg #####################################
+
