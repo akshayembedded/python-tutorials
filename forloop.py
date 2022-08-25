e5c5a5c0c13dc93c38fecf6ecfe6f08cf8119833
Akshay <embedded.akshay@gmail.com>
2022-08-25 15:15:49 +0530

    loop and function added

--- forloop.py
+++ forloop.py
@@ -1,3 +1,17 @@
 l=("12",5,5.63,3+5j)
 for i in l:
     print(i, type(i))
+
+
+for i in range(8):  #(0,1,2,3,4,5,6,7)
+    print(i)
+print("i #####################")
+for i in range(5,20):
+    print(i)
+
+
+print("##########################")
+
+for i in range(5,20,2):
+    print(i)
+
--- functions.py
+++ functions.py
@@ -17,3 +17,16 @@ testfn2(b=15,a=63)
 
 ########### Default arg #####################################
 
+print("Hello","Welcome")# sep =space end \n
+
+def testfn2(a,b=63):
+    print("\nIts def function\n Values are\na=",a)
+    print("b=",b)
+
+testfn2(15,63)
+testfn2(b=15,a=63)
+testfn2()
+testfn2(13)
+
+###############  #################
+
