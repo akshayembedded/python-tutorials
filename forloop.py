fafde7b8c70d28c3fb7fa712e30d998b094cb8e0
Akshay <embedded.akshay@gmail.com>
2022-08-26 19:24:30 +0530

    LOOP UPDATE

--- forloop.py
+++ forloop.py
@@ -15,3 +15,29 @@ print("##########################")
 for i in range(5,20,2):
     print(i)
 
+###################################
+def factfn(a):
+  """
+    if f=0 number is negative
+  """
+  f=1 
+  if a<0:
+    f=0
+  elif a==0:
+    f=1
+    
+  else:
+    for i in range(1,a+1):
+       f=f*i
+  return f
+   
+    
+while 1:
+    
+    b=int(input("Enter element to be added"))
+    r=factfn(b)
+
+    if(r):
+        print("The factorial of",b,"is",r)
+    else:
+        print("Negative number")
