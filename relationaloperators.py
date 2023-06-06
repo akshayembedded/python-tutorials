0dafcd58e32363e491c3896ea81dfea1d4a8d19d
Akshay P Kumar <93068695+akshayembedded@users.noreply.github.com>
2023-06-06 21:05:48 +0530

    Update relationaloperators.py

--- relationaloperators.py
+++ relationaloperators.py
@@ -1,1 +1,9 @@
+a=11.0
+b=3.0
+print("less than ",a<b)
+print("greater than is ",a>b)
+print("less than or equel is ",a<=b)
+print("Greater than or equel is ",a>=b)
+print("equel is ",a==b)
+print("not equel is ",a!=b)
 
